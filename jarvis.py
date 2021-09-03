51f700f13d212e80f8b21c01612b98e9836507c4
Bharath Kesari <88525593+BharathCoder@users.noreply.github.com>
2021-09-03 07:38:34 +0530

    Add Jarvis Assiistant
    This is a Jarvis like assistant

---
+++ jarvis.py
@@ -0,0 +1,23 @@
+##Importing Web browser for Chrome
+import webbrowser
+##Jarvis basic Interface
+print("Hello")
+print("Jarvis:What you want to do?")
+name = input("Command:")
+webbrowser.open("https://www.YouTube.com")
+print("Command completed successfully in 0.768 seconds")
+print("Opening YouTube in Chrome")
+##Opening my GitHub profile
+print("Next Command>?")
+name = input("Command:")
+webbrowser.open("https://www.github.com/bharathcoder")
+print("Command completed successfully in 0.685 seconds")
+print("Opening bharathcoder in Chrome")
+##Opening my fake Blog
+print("Next Command>?")
+name = input("Command:")
+webbrowser.open("https://bharathcoder.github.io/bkblog/")
+print("Command completed successfully in 0.569 seconds")
+print("Opening BK in Chrome")
+print("Thank you for Using me")
+print("😁")
